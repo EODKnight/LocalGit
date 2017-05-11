2c2d7733915f62bbf75bf324191bbf80fc518b78
evan.knight <evan.knight2506@student.owatc.edu>
2017-05-11 09:14:14 -0600

    First commit

--- LocalGit/Form1.Designer.cs
+++ LocalGit/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.helloButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // helloButton
+            // 
+            this.helloButton.Location = new System.Drawing.Point(103, 88);
+            this.helloButton.Name = "helloButton";
+            this.helloButton.Size = new System.Drawing.Size(75, 23);
+            this.helloButton.TabIndex = 0;
+            this.helloButton.Text = "Hello";
+            this.helloButton.UseVisualStyleBackColor = true;
+            this.helloButton.Click += new System.EventHandler(this.helloButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.helloButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button helloButton;
     }
 }
 
--- LocalGit/Form1.cs
+++ LocalGit/Form1.cs
@@ -16,5 +16,10 @@ namespace LocalGit
         {
             InitializeComponent();
         }
+
+        private void helloButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello!");
+        }
     }
 }
